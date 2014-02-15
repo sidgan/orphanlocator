420e08671e58a4aceb645da7ee782bbec23e6340
Zahra <zashktorab@gmail.com>
2014-02-15 19:32:52 +0400

    better logging

--- formoutput.php
+++ formoutput.php
@@ -4,7 +4,8 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>
 $.getJSON("fakeData.php?firstName=<?= $_POST[first_name]?>",function(data){
-alert(data)
+alert(JSON.stringify(data));
+console.log(data);
 
 });
 
