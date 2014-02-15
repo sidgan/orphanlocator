0c31fa79a42a18a07f4a6e76d27c7e0df55d4e88
Zahra <zashktorab@gmail.com>
2014-02-16 03:31:30 +0400

    Sdsd

--- out.php
+++ out.php
@@ -4,10 +4,15 @@
 <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
 <script>
 $.getJSON('fakeData.php?first_name=<?=$_POST[first_name]?>&last_name=<?= $_POST[last_name]?>&year_birth=<?= $_POST[year_birth]?>&year_missing=<?= $_POST[year_missing]?>&gender=<?= $_POST[gender]?>&height=<?= $_POST[height]?>&location=<?= $_POST[location]?>&additional_information=<?= $_POST[additional_information] ?>',function(data){
-$.ajax({
-    type: 'POST',
-    url: 'show_results.php',
-    data: {'myData': data}})
+
+var xhr = Ti.Network.createHTTPClient();
+xhr.open("POST", "show_results.php");
+xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+ 
+var postData = {
+    object: { child1:data}
+}
+xhr.send(postData);
 
 });
 
