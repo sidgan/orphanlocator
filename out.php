abf06d2d4400dd8bdbba07703ca1472c217f2ebe
Zahra <zashktorab@gmail.com>
2014-02-16 05:20:52 +0400

    sdsdf

--- out.php
+++ out.php
@@ -3,9 +3,9 @@
 
 <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
 <script>
-$.getJSON('fakeData.php?first_name=<?=$_POST[first_name]?>&last_name=<?= $_POST[last_name]?>&year_birth=<?= $_POST[year_birth]?>&year_missing=<?= $_POST[year_missing]?>&gender=<?= $_POST[gender]?>&height=<?= $_POST[height]?>&location=<?= $_POST[location]?>&additional_information=<?= $_POST[additional_information] ?>',function(data){
+$.getJSON('http://orphanlocator.hack-day.net/search.php?first_name=<?=$_POST[first_name]?>&last_name=<?= $_POST[last_name]?>&year_birth=<?= $_POST[year_birth]?>&year_missing=<?= $_POST[year_missing]?>&gender=<?= $_POST[gender]?>&height=<?= $_POST[height]?>&location=<?= $_POST[location]?>&additional_information=<?= $_POST[additional_information] ?>',function(data){
 
-var data_for_php = {param : data};
+var data_for_php = {param :data};
 $.ajax({
     type: "POST",
     url: "show_results.php",
--- out.php~
+++ out.php~
@@ -4,6 +4,7 @@
 <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
 <script>
 $.getJSON('fakeData.php?first_name=<?=$_POST[first_name]?>&last_name=<?= $_POST[last_name]?>&year_birth=<?= $_POST[year_birth]?>&year_missing=<?= $_POST[year_missing]?>&gender=<?= $_POST[gender]?>&height=<?= $_POST[height]?>&location=<?= $_POST[location]?>&additional_information=<?= $_POST[additional_information] ?>',function(data){
+
 var data_for_php = {param : data};
 $.ajax({
     type: "POST",
@@ -11,6 +12,7 @@ $.ajax({
     dataType: "json",
     data: data_for_php
     });
+
 });
 </script>
 </head>
