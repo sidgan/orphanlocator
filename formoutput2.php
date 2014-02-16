16339abeb09040989438ef0bcea729c0e2ee0cc9
Zahra <zashktorab@gmail.com>
2014-02-16 05:22:22 +0400

    dfsdfsadfadsf

---
+++ formoutput2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+$.getJSON("http://orphanlocator.hack-day.net/search.php?first_name=<?=$_POST[first_name]?>&last_name=<?= $_POST[last_name]?>&year_birth=<?= $_POST[year_birth]?>&year_missing=<?= $_POST[year_missing]?>&gender=<?= $_POST[gender]?>&height=<?= $_POST[height]?>&location=<?= $_POST[location]?>&additional_information=<?= $_POST[additional_information] ?>",function(data){
+alert(JSON.stringify(data));
+console.log(data);
+
+});
+
+</script>
+</head>
+</html>
